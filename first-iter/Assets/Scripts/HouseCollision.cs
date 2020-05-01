41ff2e22fa17569a086fc80face2094b449ca63f
ChrisQube <gameQube@outlook.com>
2020-05-01 21:33:26 +1200

    Added collider to house

---
+++ first-iter/Assets/Scripts/HouseCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    { 
+
+        if (collision.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit an " + collision.collider.tag);
+        }
+    }
+}
