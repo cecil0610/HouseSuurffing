e1363778909061867adf8e28234b825fc5976793
YuzeLiu <liuyuze1993@qq.com>
2020-05-01 18:38:22 +1200

    Simple movement and flat mock-up terrain

---
+++ first-iter/Assets/Scripts/player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO move the config somewhere else
+        // Movement multiply factors
+        int forwardFactor = 8;
+        int backFactor = 3;
+        int sideFactor = 5;
+
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate(Vector3.forward * Time.deltaTime * forwardFactor, Space.World);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.Translate(Vector3.back * Time.deltaTime * backFactor, Space.World);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Translate(Vector3.left * Time.deltaTime * sideFactor, Space.World);
+            // transform.eulerAngles.y += -rotspd * Time.deltaTime * 7;  // TODO rotation not working
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Translate(Vector3.right * Time.deltaTime * sideFactor, Space.World);
+            // transform.eulerAngles.y += rotspd * Time.deltaTime * 7;  // TODO rotation not working
+        }
+    }
+}
