19e8e2de9e2fe4b87f4232371467ed0ba826e4c2
ChrisQube <gameQube@outlook.com>
2020-05-01 19:26:02 +1200

    Added LeanTween and Truck Animation

---
+++ first-iter/Assets/Scripts/obstacleAnimator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacleAnimator : MonoBehaviour
+{
+    public LeanTweenType easeType;
+    public float amplitude;
+    public float movementFrequency;
+
+    private void OnEnable()
+    {
+        LeanTween.moveLocalY(gameObject, amplitude, 1/movementFrequency).setLoopPingPong().setEase(easeType);
+    }
+
+    public void OnClose()
+    {
+        Destroy(gameObject);
+    }
+}
