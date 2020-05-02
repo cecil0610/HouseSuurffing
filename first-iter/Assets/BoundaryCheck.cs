018e759d8a44c688c8a0dd1c9f01508c31d70606
ChrisQube <gameQube@outlook.com>
2020-05-02 19:25:46 +1200

    Camera lock and boundary set

---
+++ first-iter/Assets/BoundaryCheck.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryCheck : MonoBehaviour
+{
+    public float fXBoundary = 40f;
+    public float fZBoundary = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > fXBoundary)
+        {
+            transform.position = new Vector3(fXBoundary, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x < -fXBoundary)
+        {
+            transform.position = new Vector3(-fXBoundary, transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.z > fZBoundary)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, fZBoundary);
+        }
+        else if (transform.position.z < -fZBoundary)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -fZBoundary);
+        }
+    }
+}
